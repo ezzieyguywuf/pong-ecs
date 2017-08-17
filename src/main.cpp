ab5092ebcd6c3ffd17428b64da85097796b2d14e
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2017-08-17 09:51:01 -0400

    Added top and bottom wall.

--- src/main.cpp
+++ src/main.cpp
@@ -21,26 +21,39 @@
 
 int main(int argc, char ** argv) {
     ecs::Manager manager;
-    unsigned int width = 480;
-    unsigned int height = 640;
+    unsigned int width = 640;
+    unsigned int height = 480;
 
     // Make our components
+    //     ball
     sf::CircleShape ballCircleShape(15);
     ecs::ptrIComponent ballShape(new ShapeComponent(ballCircleShape));
     ecs::ptrIComponent ballPosition(new PositionComponent(0,0));
     ecs::ptrIComponent ballSpeed(new SpeedComponent(0.1,0.1));
-    //sf::Vector2f(width, 10);
-    //sf::RectangleShape wallShape(size);
-    //ecs::ptrIComponent wPtr(new ShapeComponent(wallShape));
+    //     top wall
+    sf::Vector2f size(width, 10);
+    sf::RectangleShape wall1RectShape(size);
+    ecs::ptrIComponent wall1Shape(new ShapeComponent(wall1RectShape));
+    ecs::ptrIComponent wall1Pos(new PositionComponent(0,0));
+    //     bot wall
+    sf::RectangleShape wall2RectShape(size);
+    ecs::ptrIComponent wall2Shape(new ShapeComponent(wall2RectShape));
+    ecs::ptrIComponent wall2Pos(new PositionComponent(0, height-10));
 
     // Make our entities and add the appropriate components.
     ecs::Entity ball = manager.makeEntity();
-    manager.addComponent(ball, std::move(ballShape));
-    manager.addComponent(ball, std::move(ballPosition));
-    manager.addComponent(ball, std::move(ballSpeed));
+        manager.addComponent(ball, std::move(ballShape));
+        manager.addComponent(ball, std::move(ballPosition));
+        manager.addComponent(ball, std::move(ballSpeed));
+    ecs::Entity topWall = manager.makeEntity();
+        manager.addComponent(topWall, std::move(wall1Shape));
+        manager.addComponent(topWall, std::move(wall1Pos));
+    ecs::Entity botWall = manager.makeEntity();
+        manager.addComponent(botWall, std::move(wall2Shape));
+        manager.addComponent(botWall, std::move(wall2Pos));
 
     // Instantiate our game engine
-    sf::RenderWindow rWindow(sf::VideoMode(height, width), "SFML Pong Demo");
+    sf::RenderWindow rWindow(sf::VideoMode(width, height), "SFML Pong Demo");
     Display display(rWindow);
     Engine engine(display);
 
